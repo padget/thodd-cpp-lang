4673dbfb5522512516c214c6d1478a7673cd1be0
padget <padget.pro@gmail.com>
2017-07-25 20:57:51 +0200

    lambda style

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,7 @@
 #include <list>
 #include <functional>
 
-#include <thodd/tuple/algorithm.hpp>
-#include <thodd/lang.hpp>
+#include <thodd/functional.hpp>
 
 enum class test { foo, bar, fii } ;
 
@@ -13,150 +12,57 @@ enum class calc { integer, add_op, mult } ;
 
 enum class lexword {digit, point, float_, exp} ;
 
-constexpr auto 
-predict (
-    thodd::syntax::word<auto, auto> const & ... __word)
+
+
+
+template<typename char_t>
+struct bin_op
 {
-    __word.reg
-} 
+    char_t left, 
+    char_t op, 
+    char_t right,
+} ;
 
-constexpr auto lex = 
-    [] ( auto&& __begin, 
-         auto&& __end, 
-         thodd::syntax::word<auto, auto> const & ... __word) 
-    {
 
-    } ;
 
 int main() 
 {
-    using namespace thodd::regex ;
+    using namespace thodd ;
 
-    std::string __input("1") ;
-    auto __begin = __input.begin() ;
-    auto __end = __input.end() ;
+    auto is_between_chars = tern($0 <= *$2 && *$2 <= $1)[cout_<<*$2, ++$2][$2] ;
+    auto is_particular_char = tern($0 == *$1)[cout_<<*$1, ++$1][$1] ;
 
-    std::cout << std::boolalpha 
-              << matches(chr('0') - chr('9'), __begin, __end)
-              << std::endl ;
+    auto is_digit = bind(is_between_chars, val('0'), val('9'), $0) ;
 
-    __begin = __input.begin() ;
+    auto is_add  = bind(is_particular_char, val('+'), $0) ;
+    auto is_sub  = bind(is_particular_char, val('-'), $0) ;
+    auto is_mult = bind(is_particular_char, val('*'), $0) ;
+    auto is_div  = bind(is_particular_char, val('/'), $0) ;
 
-    std::cout << std::boolalpha 
-              << matches(!chr('2'), __begin, __end)
-              << std::endl ;
-
-    __begin = __input.begin() ;
-
-    std::cout << std::boolalpha 
-              << matches(chr('2') | chr('3') | chr('1'), __begin, __end)
-              << std::endl ;
-
-    std::string __input2 = "231" ;
-    
-    std::tie(__begin, __end) = std::tuple(__input2.begin(), __input2.end()) ;
-
-    std::cout << std::boolalpha 
-              << matches(chr('2') > chr('3') > chr('1'), __begin, __end)
-              << std::endl ;
-
-    std::string __input3 {"11111"} ;
-    __begin = __input3.begin() ;
-    __end = __input3.end() ;
-
-    std::cout << std::boolalpha
-              << matches((*chr('1'))(2, 5), __begin, __end)
-              << std::endl ;
-
-    std::string __input4 { "my_name_is_bryan..." } ;
-    __begin = __input4.begin() ;
-    __end = __input4.end() ;
-
-    constexpr auto __odod = +(chr('a') - chr('z') | chr('_')) > chr('.') > chr('.') > chr('.');
-    std::cout << std::boolalpha 
-              << matches(
-                  __odod, 
-                  __begin, 
-                  __end) 
-              << std::endl ;
-
-
-    namespace syntax = thodd::syntax ;
-    
-    constexpr auto __23 = syntax::wrd(__odod, test::fii) ;
-    constexpr auto __or = __23 | __23 | __23 ;
-    constexpr auto __and = __23 > __23 > __23 ;
-    constexpr auto __some = *__23 ;
-    constexpr auto __comb = +__and | __and ;
-
-    __begin = __input4.begin() ;
-    __end = __input4.end() ;
-    std::list<syntax::token<test, decltype(__begin)>> __tokens ;
-    
-    read(__23, __begin, __end, __tokens) ;
-    
-    if (!__tokens.empty())
-        for (auto&& __token : __tokens)
-            for (auto&& __item : __token)
-                std::cout << __item ;
-
-
-    auto __tuple = std::tuple(1, 2, 3) ;
-    auto __accumulator = [] (auto&& __state, auto&& __step) {return __state + __step ; } ;
-    auto __func = [](auto&& __item) -> decltype(auto) { return __item ; } ;
-
-    ::thodd::tuple::iterate(
-        [] (auto && __item) -> auto&& {return (std::cout << __item) ; }, 
-        __tuple) ;
-
-    ::thodd::tuple::iterate_if(
-        [] (auto && __item) { return __item % 2 == 0 ; } ,  
-        [] (auto && __item) { std::cout << __item ; }    ,
-        std::tuple(1, 2, 3, 4, 5, 6, 7, 8, 9)) ;
-    
-    std::cout << ::thodd::tuple::accumulate(__func, __accumulator, 0u, __tuple) << std::endl ;
-    std::cout << ::thodd::tuple::accumulate_if([](auto && __state, auto && __item) { return __item % 2 == 0 ; } , __func, __accumulator, 0u, __tuple) << std::endl ;
-
-    std::string __input5 {"my_name_is_bryan..."} ;
-    __begin = __input5.begin() ;
-    __end = __input5.end() ;
-    std::list<syntax::token<test, decltype(__begin)>> __tokensbad ;
-    
-    
-    read(__23, __begin, __end, __tokensbad) ;
-    
-    if (!__tokensbad.empty())
-    {
-        for (auto&& __token : __tokensbad)
-            if (!__token.invalid())
-                for (auto&& __item : __token)
-                    std::cout << __item ;
-            else std::cout << "token invalid" << std::endl ;
-    } 
-
-    std::cout << "next test" << std::endl ;
-
-    constexpr auto __and23 = __23 > __23 > __23 ;
-
-    std::string __input6 {"my_name_is_bryan...my_name_is_bryan...my_name_is_bryan.."} ;
-    __begin = __input6.begin() ;
-    __end = __input6.end() ;
-    std::list<syntax::token<test, decltype(__begin)>> __tokensgood ;
-
-    read(__and23, __begin, __end, __tokensgood) ;
-
-    for (auto&& __token : __tokensgood)
-        if (!__token.invalid())
-            for (auto&& __item : __token)
-                std::cout << __item ;
-        else std::cout << "token invalid" << std::endl ;
+    auto is_open  = bind(is_particular_char, val('('), $0) ;
+    auto is_close = bind(is_particular_char, val(')'), $0) ;
 
     
+    auto is_add_operation = compose(is_digit, compose(is_add, is_digit)) ;
+    auto is_sub_operation = compose(is_digit, compose(is_sub, is_digit)) ;
+    auto is_mult_operation = compose(is_digit, compose(is_mult, is_digit)) ;
+    auto is_div_operation = compose(is_digit, compose(is_div, is_digit)) ;
 
-    lex(__begin, __end, 
-        syntax::wrd(chr('0') - chr('9')                                                  , lexword::digit), 
-        syntax::wrd(chr('.')                                                             , lexword::point), 
-        syntax::wrd(+(chr('0') - chr('9')) > (~(chr('.') > +(chr('0') - chr('9'))))(0, 1), lexword::float_)) ;
+    auto is_expression = compose(
+                            is_add_operation, 
+                            compose(
+                                is_sub_operation, 
+                                compose(
+                                    is_mult_operation, 
+                                    is_div_operation))) ;
+
+    auto char_to_int = bind(static_cast_<int>, *$0 + val('0')) ;
+    auto on_add_operation = compose(char_to_int, ) ;
+
+    std::string __str {"1/1="}; 
+    std::cout << *is_expression(__str.begin()) ;
+
+  
 
     
 }

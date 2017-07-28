4263533cfe87340d140844cb18db3c92f28be64f
padget <padget.pro@gmail.com>
2017-07-28 16:43:08 +0200

    analyse first

--- include/thodd/lang/lexical/analyse.hpp
+++ include/thodd/lang/lexical/analyse.hpp
@@ -1,6 +1,8 @@
 #ifndef __THODD_LANG_LEXICAL_ANALYSE_HPP__
 #  define __THODD_LANG_LEXICAL_ANALYSE_HPP__
 
+#  include <array>
+
 #  include <thodd/lang/lexical/token.hpp>
 
 namespace 
@@ -9,10 +11,36 @@ thodd::lang::lexical
     inline auto
     analyse(
         auto __begin, 
-        auto __end, 
-        auto const & ... __reg)
+        auto __end,
+        auto const & __first, 
+        auto const & ... __word)
     {
-        
+        auto __token = make_token(__first.id, __end, __end) ;
+
+        for(auto&& __tmp : std::array  { 
+                                __analyse(__begin, __end, __first), 
+                                __analyse(__begin, __end, __word)... })
+        {
+            auto&& __token_size = std::distance(__token.data.first, __token.data.second) ;
+            auto&& __tmp_size = std::distance(__tmp.data.first, __tmp.data.second) ;
+            
+            if(__token_size < __tmp_size)
+                __token = __tmp ;
+        }
+
+    }
+
+    inline auto 
+    __analyse (
+        auto __begin, 
+        auto __end, 
+        auto const & __word)
+    {
+        auto __save = __begin, 
+        return 
+        matches(__begin, __end, __reg) ?
+        make_token(__word.id, __save, __begin) :
+        make_invalid_token(__word.id, __save, __begin) ;
     }
 }
 

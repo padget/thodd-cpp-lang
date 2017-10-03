56f334263ec41f791bb4dc1a24736585649fc3c8
padget <padget.pro@gmail.com>
2017-10-03 20:18:55 +0200

    build_tokens in lambda

--- include/thodd/lang/build_tokens.hpp
+++ include/thodd/lang/build_tokens.hpp
@@ -10,42 +10,39 @@
 namespace 
 thodd::lang
 {
-    template <
-        typename language_t>
-    inline auto
-    build_tokens (
-        auto __begin, 
-        auto const end,
-        auto ... terminal)
+    inline constexpr auto
+    token_builder = 
+    [] (auto ... terminal) 
     {
-        std::list<token<language_t, decltype(__begin)>> __tokens ;
-
-        auto cursor = __begin ;
-        
-        while (cursor != end)
+        return
+        [terminal...] (auto begin, auto const end)
         {
-            auto&& __each_tokens = 
-                    std::array {std::move(terminal(cursor, end)) ... } ;
-      
-            auto&& __greater = 
-                std::max_element(
-                    __each_tokens.begin(), __each_tokens.end(), 
-                    [] (auto const & __l, auto const & __r) 
-                    { return __l.size() < __r.size() ; }) ;
-
-            if (cursor == (*__greater).end())
-               __tokens.push_back(token { std::pair { cursor, ++cursor }, language_t::error }) ;  
-            else 
-            {
-                cursor = (*__greater).end() ;
+            constexpr auto greater_token = 
+            [] (auto const & __l, auto const & __r) { return __l.size() < __r.size() ; } ;
                 
-                if ((*__greater).id != language_t::ignored) 
-                    __tokens.push_back(std::move(*__greater)) ;
-            }
-        }
+            std::list<token<language_t, decltype(__begin)>> __tokens ;
+            auto cursor = __begin ;
+            
 
-        return __tokens ;
-    }
+            while (cursor != end)
+            {
+                auto&& __each_tokens = std::array {std::move(terminal(cursor, end)) ... } ;
+                auto&& __greater = std::max_element(__each_tokens.begin(), __each_tokens.end(), greater_token) ;
+
+                if (cursor == (*__greater).end())
+                   __tokens.push_back(token { std::pair { cursor, ++cursor }, language_t::error }) ;  
+                else 
+                {
+                    cursor = (*__greater).end() ;
+
+                    if ((*__greater).id != language_t::ignored) 
+                        __tokens.push_back(std::move(*__greater)) ;
+                }
+            }
+
+            return __tokens ;
+        } ;
+    } ;
 }
 
 #endif

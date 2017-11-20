83bae22995dd8b1acf4bf81162f252058e6e33cb
padget <padget.pro@gmail.com>
2017-11-20 09:40:21 +0100

    clean code

--- include/thodd/lang2/build_tokens.hpp
+++ include/thodd/lang2/build_tokens.hpp
@@ -7,30 +7,9 @@
 #  include <thodd/optional.hpp>
 #  include <thodd/lang2/token.hpp>
 
-#  include <cxxabi.h>
-
-template<
-    typename T>
-std::string type_name()
-{
-    int status;
-    std::string tname = typeid(T).name();
-    char *demangled_name = abi::__cxa_demangle(tname.c_str(), NULL, NULL, &status);
-    
-    if(status == 0) 
-    {
-        tname = demangled_name;
-        std::free(demangled_name);
-    } 
-
-    return tname;
-}
-
-
 namespace 
 thodd::lang
 {
-
   constexpr auto token_greater = 
   [] (auto && ltoken, auto && rtoken) 
   { 
